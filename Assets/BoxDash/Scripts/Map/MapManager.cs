b556d57414fe28c380775c2b86a890682b18a441
LarryThe4th <chinyikusougen@gmail.com>
2016-10-27 09:52:54 +0900

    Create a new repository for the project
    Removed all the assets but the scripts.
    Once the project finished i will update the related assets.

---
+++ Assets/BoxDash/Scripts/Map/MapManager.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utility;
+
+namespace BoxDash.Map {
+    /// <summary>
+    /// This class manager all the map content in game.
+    /// Included map tile gameObject loading and danmacy map creation.
+    /// </summary>
+    public class MapManager : Singleton<MapManager>
+    {
+        #region Private variable
+        // ---------- Private variable ----------
+        private GameObject m_MapTilePrefab;
+        private static readonly float m_TileLengthInHalf = 0.254f;
+        // Calualate the diagonal line across the tile qube, as long as the
+        // tile is a square, the diagonal line's lenght should be (2^2 * (length of side)). 
+        private static readonly float m_TileOffset = Mathf.Sqrt(2) * m_TileLengthInHalf;
+
+        #endregion
+
+        #region Private methods
+        /// <summary>
+        /// Use this for initialization.
+        /// </summary>
+        private void Start() {
+            LoadGameResource("Tile_white", out m_MapTilePrefab);
+
+            CreateMap();
+        }
+
+        private void CreateMap() {
+            Vector3 rotation = new Vector3(-90, 45, 0);
+            for (int rowIndex = 0; rowIndex < 10; rowIndex++)
+            {
+                for (int columnIndex = 0; columnIndex < 5; columnIndex++)
+                {
+                    GameObject tile =Instantiate(
+                        m_MapTilePrefab, 
+                        new Vector3(columnIndex * m_TileOffset, 0, rowIndex * m_TileOffset), 
+                        Quaternion.Euler(rotation)) as GameObject;
+                    tile.transform.SetParent(this.transform);
+                }
+            }
+        }
+
+        /// <summary>
+        /// A overload method of the LoadGameResource<T>(...),
+        /// T will be set as "Object" as default.
+        /// </summary>
+        /// <param name="resourceName">The name of the resource.</param>
+        /// <param name="resouce">The resouce object you pass in, return NULL if load failed.</param>
+        /// <param name="resourceFolder">The specific folder under resources folder.</param>
+        private void LoadGameResource(string resourceName, out GameObject resouce, string resourceFolder = "")
+        {
+            LoadGameResource<Object>(resourceName, out resouce, resourceFolder);
+        }
+
+        /// <summary>
+        /// Use this method to load resources from the unity3D specific resources folder
+        /// </summary>
+        /// <typeparam name="T">The type of object this you wanna load.</typeparam>
+        /// <param name="resourceName">The name of the resource.</param>
+        /// <param name="resouce">The resouce object you pass in, return NULL if load failed.</param>
+        /// <param name="resourceFolder">The specific folder under resources folder.</param>
+        private void LoadGameResource<T>(string resourceName, out GameObject resouce, string resourceFolder = "") where T : Object {
+            if (!string.IsNullOrEmpty(resourceFolder))
+            {
+                resouce = Resources.Load<T>(resourceFolder + "/" + resourceName) as GameObject;
+            }
+            else {
+                resouce = Resources.Load<T>(resourceName) as GameObject;
+            }
+#if UNITY_EDITOR
+            if (resouce == null) {
+                if (string.IsNullOrEmpty(resourceFolder))
+                {
+                    Debug.LogError("Failed to load resouce " + resourceName +
+                            " from Resources Folder.");
+                }
+                else {
+                    Debug.LogError("Failed to load resouce " + resourceName +
+                            " from " + resourceFolder + " Folder.");
+                }
+            }
+#endif
+        }
+        #endregion
+    }
+}
+
---
+++ Assets/BoxDash/Scripts/Utility/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Utility {
+    public class Singleton<T> : MonoBehaviour
+        where T : Component
+    {
+        private static T m_Instance;
+        public static T Instance
+        {
+            get
+            {
+                if (m_Instance == null)
+                {
+                    var objs = FindObjectsOfType(typeof(T)) as T[];
+                    if (objs.Length > 0)
+                        m_Instance = objs[0];
+                    if (objs.Length > 1)
+                    {
+                        Debug.LogError("There is more than one " + typeof(T).Name + " in the scene.");
+                    }
+                    if (m_Instance == null)
+                    {
+                        GameObject obj = new GameObject();
+                        obj.hideFlags = HideFlags.HideAndDontSave;
+                        m_Instance = obj.AddComponent<T>();
+                    }
+                }
+                return m_Instance;
+            }
+        }
+    }
+}
